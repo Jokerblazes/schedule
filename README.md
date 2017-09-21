96ed08941db5bfcad871a3c08d7fcb5dbab9a479
Jokerblazes <zcd123@qq.com>
2017-09-21 13:28:41 +0800

    md-revise

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # schedule
 定时任务框架
+
+## 功能
+
+* 扫描初始化一系列的定时任务
+* 运行过程中动态的加入、删除定时任务
+* 用户可以自定义定时策略并可对策略进行缓存
+
+## 定时表达式
+
+例：`1,7,11,12`
+
+周一日至周六的每天11点12分执行
+
+## demo
+
+方式一
+
+```java
+	@Scheduled(cron = "1,7,10,45")
+	public void test1() {
+		System.out.println("do test1");
+	}
+```
+
+方式二：
+
+```java
+public void testSchedule() {
+		ScheduleHelper helper = ScheduleFactory.getScheduleHelper(PoolScheduleStrategy.class);
+		Runnable runnable = new Runnable() {
+			public void run() {
+				System.out.println(Thread.currentThread().getName());
+				throw new RuntimeException();
+			}
+		};
+		ScheduleEntity entity = new ScheduleEntity("1,7,11,11", runnable);
+
+		helper.addSchedule(entity);
+	}
+```
+
+
+
--- src/test/java/com/joker/schedule/AppTest.java
+++ src/test/java/com/joker/schedule/AppTest.java
@@ -11,35 +11,34 @@ import com.joker.schedule.strategy.PoolScheduleStrategy;
  */
 public class AppTest {
 	
-//	@Test
-//	public void testSchedule() {
-//		ScheduleHelper helper = ScheduleFactory.getScheduleHelper(PoolScheduleStrategy.class);
-//		System.out.println(helper);
-//		Runnable runnable = new Runnable() {
-//			public void run() {
-//				System.out.println(Thread.currentThread().getName());
-//				throw new RuntimeException();
-//			}
-//		};
-//		ScheduleEntity entity = new ScheduleEntity("1,7,11,11", runnable);
-//		Runnable runnable1 = new Runnable() {
-//
-//			public void run() {
-//				System.out.println(Thread.currentThread().getName());
-//				System.out.println(222);
-//			}
-//		};
-//		ScheduleEntity entity1 = new ScheduleEntity("1,7,11,12", runnable1);
-//
-//		helper.addSchedule(entity);
-//		helper.addSchedule(entity1);
-//		try {
-//			Thread.sleep(70000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//		helper.removeSchedule(entity);
-//	}
+	@Test
+	public void testSchedule() {
+		ScheduleHelper helper = ScheduleFactory.getScheduleHelper(PoolScheduleStrategy.class);
+		Runnable runnable = new Runnable() {
+			public void run() {
+				System.out.println(Thread.currentThread().getName());
+				throw new RuntimeException();
+			}
+		};
+		ScheduleEntity entity = new ScheduleEntity("1,7,11,11", runnable);
+		Runnable runnable1 = new Runnable() {
+
+			public void run() {
+				System.out.println(Thread.currentThread().getName());
+				System.out.println(222);
+			}
+		};
+		ScheduleEntity entity1 = new ScheduleEntity("1,7,11,12", runnable1);
+
+		helper.addSchedule(entity);
+		helper.addSchedule(entity1);
+		try {
+			Thread.sleep(70000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		helper.removeSchedule(entity);
+	}
 //	
 //	@Test
 //	public void testDate() {
